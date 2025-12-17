055d5e619d4b157c77d16047a114f0bb4d847262
T Do <tdo02@jaguar.tamu.edu>
2025-12-17 12:34:47 -0600

    fix: ejs layout rendering

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home')

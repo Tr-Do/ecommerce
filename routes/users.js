0224bd2dd404493c1c9782561cc1c232ad0fad37
T Do <tdo02@jaguar.tamu.edu>
2026-01-04 13:14:03 -0600

    feat: add login page

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -17,7 +18,15 @@ router.post('/register', async (req, res) => {
         req.flash('error', e.message);
         res.redirect('/register');
     }
-
 })
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back');
+    res.redirect('/');
+});
 
 module.exports = router;

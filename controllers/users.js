0736bf779e30750b58f5c98d111be599bce865be
T Do <tdo02@jaguar.tamu.edu>
2026-01-06 16:49:28 -0600

    feat: set flash message upon login, set bottom margin of register, sign in buttons

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const router = express.Router();
 const User = require('../models/user');
-const passport = require('passport');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-})
-
-router.post('/register', async (req, res) => {
+module.exports.renderRegister = async (req, res) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
@@ -18,26 +11,22 @@ router.post('/register', async (req, res) => {
         req.flash('error', e.message);
         res.redirect('/register');
     }
-})
+}
 
-router.get('/login', (req, res) => {
+module.exports.renderLogin = (req, res) => {
     if (!req.session.returnTo && req.headers.referer) {
         req.session.returnTo = new URL(req.headers.referer).pathname;
     }
     res.render('users/login');
-});
+}
 
-router.post(
-    '/login',
-    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', keepSessionInfo: true }), (req, res) => {
-        req.flash('success', 'Welcome back');
-        const redirectUrl = req.session.returnTo || '/products';
-        delete req.session.returnTo;
-        res.redirect(redirectUrl);
-    }
-);
+module.exports.login = (req, res) => {
+    const redirectUrl = req.session.returnTo || '/products';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
 
-router.get('/logout', (req, res, next) => {
+module.exports.logout = (req, res, next) => {
     const redirectUrl = req.headers.referer || '/products';
 
     req.logout(function (err) {
@@ -47,6 +36,4 @@ router.get('/logout', (req, res, next) => {
         req.flash('success', 'Good bye!');
         res.redirect(redirectUrl);
     });
-})
-
-module.exports = router;
+}

feab0acd4270f8532e994936a863efc9c62ebea8
T Do <tdo02@jaguar.tamu.edu>
2026-01-10 10:38:14 -0600

    feat: add product size to product new page, schema and joi validation

--- schemas.js
+++ schemas.js
@@ -5,7 +5,8 @@ module.exports.productSchema = Joi.object({
         name: Joi.string().required(),
         price: Joi.number().required().min(0),
         description: Joi.string().required(),
-        image: Joi.string().allow('').optional()
+        image: Joi.string().allow('').optional(),
+        size: Joi.array().items(Joi.string().valid('S', 'M', 'L', 'XL')).optional()
     }).required(),
-    deleteImages: Joi.array()
+    deleteImages: Joi.array(),
 })

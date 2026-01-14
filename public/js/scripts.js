71fd6a06ff1a6ff0c656683e7f1cb93072c6d9f8
T Do <tdo02@jaguar.tamu.edu>
2026-01-14 17:34:32 -0600

    fix: cart count logic on home page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,25 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const savedScroll = sessionStorage.getItem("scrollY");
     if (savedScroll !== null) {
-        window.scrollTo(0, parseInt(savedScroll, 12));
+        window.scrollTo(0, parseInt(savedScroll, 10));
         sessionStorage.removeItem("scrollY");
     }
-});
 
-const addToCartBtn = document.getElementById('add-to-cart');
-const cartCount = document.getElementById('cart-count');
+    // increase cart count logic
+    const addToCartBtn = document.getElementById('add-to-cart');
+    const cartCount = document.getElementById('cart-count');
 
-if (cartCount) {
-    const saved = localStorage.getItem('cartCount');
-    if (saved !== null) {
-        cartCount.textContent = saved;
+    if (cartCount) {
+        const saved = localStorage.getItem('cartCount');
+        if (saved !== null) {
+            cartCount.textContent = saved;
+        }
     }
-}
 
-if (addToCartBtn && cartCount) {
-    addToCartBtn.addEventListener('click', () => {
-        const num = Number(cartCount.textContent) + 1;
-        cartCount.textContent = String(num);
-        localStorage.setItem('cartCount', String(num));
-    })
-}
+    if (addToCartBtn && cartCount) {
+        addToCartBtn.addEventListener('click', () => {
+            const num = Number(cartCount.textContent) + 1;
+            cartCount.textContent = String(num);
+            localStorage.setItem('cartCount', String(num));
+        })
+    }
+});

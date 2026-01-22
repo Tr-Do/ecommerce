2300986b40cf9db2c4d6b1d55040dde6e38302f2
T Do <tdo02@jaguar.tamu.edu>
2026-01-21 22:06:45 -0600

    fix: invalidate messages on inputs

--- public/js/validatedForm.js
+++ public/js/validatedForm.js
@@ -1,18 +1,16 @@
+// public/js/validatedForm.js
 (function () {
-    'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.validated-form')
+    'use strict';
 
-    // Loop over them and prevent submission
-    Array.from(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+    const forms = document.querySelectorAll('.validated-form');
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-})()
+    Array.from(forms).forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();

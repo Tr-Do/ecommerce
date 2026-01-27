73bb3431bf1d29f37e1d922bada778aad7347305
T Do <tdo02@jaguar.tamu.edu>
2026-01-27 00:17:45 -0600

    feat: display file names before upload

--- public/js/uploadToggle.js
+++ public/js/uploadToggle.js
@@ -17,3 +17,34 @@ document.querySelectorAll('.size-check').forEach(f => {
         target.classList.toggle('d-none', !f.checked);
     })
 });
+
+document.getElementById('standard').addEventListener('change', (e) => {
+    if (!e) return;
+
+    const list = document.getElementById('file');
+    if (!list) return;
+    list.innerHTML = '';
+
+    Array.from(e.target.files).forEach(file => {
+        const li = document.createElement('li');
+        li.textContent = file.name;
+        list.appendChild(li);
+    })
+})
+
+document.querySelectorAll('.fileUpload').forEach(input => {
+    input.addEventListener('change', () => {
+        const size = input.dataset.size;
+        if (!size) return;
+
+        const list = document.getElementById(`file-${size}`);
+        if (!list) return;
+        list.innerHTML = '';
+
+        Array.from(input.files).forEach(file => {
+            const li = document.createElement('li');
+            li.textContent = file.name;
+            list.appendChild(li);
+        });
+    });
+});

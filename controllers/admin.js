fa828921807fe4ce7446a478531bcd5b1cf48970
T Do <tdo02@jaguar.tamu.edu>
2026-01-31 20:43:09 -0600

    feat: add order detail route and controller

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,3 +4,10 @@ module.exports.renderView = async (req, res) => {
     const orders = await Order.find({});
     res.render('orders/adminView', { orders });
 }
+
+module.exports.orderDetail = async (req, res) => {
+    const { orderNumber } = req.params;
+    const order = Order.find(orderNumber);
+
+    res.render('orders/orderDetail', { order });
+}
--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { requireLogin, isAdmin } = require('../middleware');
 const admin = require('../controllers/admin');
 
-router.get('/adminView', requireLogin, isAdmin, admin.renderView)
+router.get('/adminView', requireLogin, isAdmin, admin.renderView);
+
+router.get(`/{orderNumber}`, requireLogin, isAdmin, admin.orderDetail);
 
 module.exports = router;

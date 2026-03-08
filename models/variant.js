f37996ae9b06223875ba7b77376d138c2b7652c7
T Do <tdo02@jaguar.tamu.edu>
2026-03-08 13:31:37 -0500

    feat: add XS size to schema

--- models/variant.js
+++ models/variant.js
@@ -1,45 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const fileSchema = new Schema({
+const fileSchema = new Schema(
+  {
     bucket: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     key: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     originalName: String,
     contentType: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    size: Number
-}, { _id: false });
+    size: Number,
+  },
+  { _id: false },
+);
 
-const variantSchema = new Schema({
+const variantSchema = new Schema(
+  {
     productId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Design',
-        required: true,
-        index: true
+      type: Schema.Types.ObjectId,
+      ref: "Design",
+      required: true,
+      index: true,
     },
     size: {
-        type: String,
-        enum: ['Standard', 'S', 'M', 'L', 'XL'],
-        required: true
+      type: String,
+      enum: ["Standard", "XS", "S", "M", "L", "XL"],
+      required: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     files: {
-        type: [fileSchema],
-        default: []
-    }
-},
-    { timestamps: true });
+      type: [fileSchema],
+      default: [],
+    },
+  },
+  { timestamps: true },
+);
 
 variantSchema.index({ productId: 1, size: 1 }, { unique: true });
-module.exports = mongoose.model('Variant', variantSchema);
+module.exports = mongoose.model("Variant", variantSchema);
